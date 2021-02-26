577dbc168e440f2d6c240774b428e642a64d58fd
cx1964 <cx1964@mail.com>
2021-02-26 15:11:03 +0100

    script z7

---
+++ 7zipBladmuziek.sh
@@ -0,0 +1,4 @@
+# file:7ZipBladmuziek.sh
+
+# 7zip de files in de directory /home/claude/Downloads/cx1964BladMuziek in de archive bladmuziek.7z
+7z a Bladmuziek /home/claude/Downloads/cx1964BladMuziek
