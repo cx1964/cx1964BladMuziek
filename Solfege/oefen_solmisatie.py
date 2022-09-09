2f08ed3aa1de52a50ddcda8173529b93b7de5488
cx1964 <cx1964@mail.com>
2022-09-09 16:46:11 +0200

    initiele versie

---
+++ Solfege/oefen_solmisatie.py
@@ -0,0 +1,11 @@
+# file: oefen_solmisatie.py
+# functie: script die random een solmisatie syllable toont om te oefen met benoemen
+
+import random
+maxNumber = 10
+
+for i in range(maxNumber):
+  randomInt = round(random.uniform(1, 7))
+  print ("Random:", randomInt) 
+
+
