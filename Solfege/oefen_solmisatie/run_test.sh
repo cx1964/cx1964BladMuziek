68aafd7774bd3285ea353e6888d0484b871eb0af
cx1964 <cx1964@mail.com>
2022-09-18 14:47:31 +0200

    voeg een voorstudie toe

---
+++ Solfege/oefen_solmisatie/run_test.sh
@@ -0,0 +1,5 @@
+# file: run_test.sh
+# function: shell script to run van python script run_test.py
+# zie https://medium.com/@autoferrit/how-to-use-pyenv-pipenv-for-python-virtual-environments-fe70459a4037
+
+pipenv run python run_test.py 
