3eec74b4da6354d554875257ff1acbfb69568350
cx1964 <cx1964@mail.com>
2022-09-26 21:48:55 +0200

    Bijna goed

--- Solfege/oefen_solmisatie/application.py
+++ Solfege/oefen_solmisatie/application.py
@@ -28,7 +28,7 @@ import random
 app = Flask(__name__)
 #app.config['UPLOAD_FOLDER'] = PICTURE_FOLDER
 
-path="/home/claude/Desktop/Downloads/cx1964BladMuziek/Solfege/oefen_solmisatie/"
+#path="/home/claude/Desktop/Downloads/cx1964BladMuziek/Solfege/oefen_solmisatie/"
 maxNumber = 10
 ssList = [] # list met solmisatie syllable pictures (inclusief pad)
 chsList = [] # list met Cruwens' handsigns pictures (inclusief pad)
@@ -37,34 +37,26 @@ chsList = [] # list met Cruwens' handsigns pictures (inclusief pad)
 @app.route('/')
 @app.route('/index')
 def show_index():
-    randomInt = round(random.uniform(1, 7))
+
     #print ("Random:", randomInt) 
     # lees een plaatje in en toon het plaatje
     # er zijn plaatjes van 1 t/m 7 met de solmisatie syllable
     # dus een plaatje van do, re, mi, enze op de notenbalk met filenamen 1ss.png t/m 7ss.png
     # er zijn ook plaatjes van de handsigns van 1chs.png t/m 7chs.png
 
-    # Bouw 2 lists op met filenamen van pictures
+    # Bouw 1 lists op met random nummers
     for i in range(maxNumber):
-      # maak een random filenaam met pad tbv de Solmisatie syllable pictures
-      fSS = path + str(randomInt) + 'ss.png' 
-      ssList.append(fSS)
+        randomInt = round(random.uniform(1, 7))
+        ssList.append(str(randomInt))
 
-      # maak een random filenaam met pad tbv de Cruwens' handsigns pictures
-      fCHS = path + str(randomInt) +'chs.png'
-      chsList.append(fCHS)
+    """
+    # maak een random filenaam met pad tbv de Solmisatie syllable pictures
+    fSS = path + str(randomInt) + 'ss.png' 
+    ssList.append(fSS)
 
-
-
-    """"
-    ToDo:
-    Onderstaande regels werken nog niet 
-    Iets met dirctory of file
-    nog uitzoeken. 
-    Doel is hoe kan je plaatjes laten zin in Flask?
-
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], fSS)
-    return render_template("index.html", user_image = full_filename)
+    # maak een random filenaam met pad tbv de Cruwens' handsigns pictures
+    fCHS = path + str(randomInt) +'chs.png'
+    chsList.append(fCHS)
     """
 
     # return "Ik ben application.py @ localhost:5000/index"
@@ -73,7 +65,7 @@ def show_index():
     # zie ook https://flask.palletsprojects.com/en/2.2.x/tutorial/templates/?highlight=template
     # zie ook https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application
     # zie ook jinja https://jinja.palletsprojects.com/en/3.1.x/
-    return render_template("index.html", ssList=ssList ) #: probeer eerst met 1 list later met fCHS=chsList)
+    return render_template("index.html", ssList=ssList ) 
     #return render_template("index.html") # zonder parameters
 
 if __name__=='__main__':
