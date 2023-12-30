2f0a9b51cad37ac555eb76a16d3c11c068da406e
cx1964 <cx1964@mail.com>
2023-12-30 20:45:49 +0100

    convert mscz -> musicxml -> ly

---
+++ Klassiek/Bach_CPE/la_caroline/convert_musicxml_to_lilypond.sh
@@ -0,0 +1,6 @@
+# file: convert_musicxml_to_lilypond.sh
+# functie: convert een musicxml file naar lilypond format
+# opmerking: parameter $1 is een file met extentie .musicxml
+#            en creeert een script met extentie .ly
+
+musicxml2ly $1
